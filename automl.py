0ee2410b2380f63eb0b56e86315b2dc9a30561cb
Gedarafi <german.drf@gmail.com>
2020-05-15 17:29:54 -0500

    Add file as an argument

--- automl.py
+++ automl.py
@@ -1,7 +1,8 @@
 import time
+import sys
 
 for position in range(50):
-    with open('drawings/marc.txt') as f:
+    with open(f'drawings/{sys.argv[1]}.txt') as f:
         object_ = f.read().strip()
     print(f'{" " * position} {object_}', end='\t')
     time.sleep(.01)

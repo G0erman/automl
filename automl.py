9294175e70bc27455f97cab23d82e1e4c428d576
Gedarafi <german.drf@gmail.com>
2020-05-16 10:18:17 -0500

    Add platform

--- automl.py
+++ automl.py
@@ -2,7 +2,8 @@ import time
 import sys
 
 for position in range(50):
+    spaces = " " * position
     with open(f'drawings/{sys.argv[1]}.txt') as f:
         object_ = f.read().strip()
-    print(f'{" " * position} {object_}', end='\t')
-    time.sleep(.01)
+    print(f'/\{spaces}{object_}', end='\r')
+    time.sleep(.1)
